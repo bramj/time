8af9b56b1ff2654fcc29b94a32a469f17abc1bc1
Bram Jans <ik@bramjans.com>
2017-04-11 08:36:54 +0200

    Initial commit (create-react-app)

---
+++ src/components/Question.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Question extends React.Component {
+
+  render() {
+    return (
+      <h2>
+        {this.props.question}
+      </h2>
+    )
+  }
+
+}
+
+export default Question
