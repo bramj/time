adb11b693464d96f9e1beacd6b9f2c075bdd8c5a
Bram Jans <ik@bramjans.com>
2017-04-12 18:14:56 +0200

    Display first question!

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import Question from './components/Question'
 import './styles.min.css'
 
 class App extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      questions: require('./questions.json'),
+      current_question_index: 0
+    }
+  }
+
+  currentQuestion () {
+    return this.state.questions[this.state.current_question_index]
+  }
+
   render() {
     return (
-      <div className="App">
-        <Question question="Is this a question?" />
+      <div className="container">
+        <div className="clearfix">
+          <Question question={this.currentQuestion().question}
+                    answers={this.currentQuestion().answers} />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import { Button, Row, Col, Form, Radio } from 'elemental'
 
 class Question extends React.Component {
-
-  render() {
-    return (
-      <h2>
-        {this.props.question}
-      </h2>
-    )
+  constructor (props) {
+    super (props)
   }
 
+  render() {
+    const answers = this.props.answers.map((answer) =>
+      <Radio name="answers" label={answer.label} value={answer.value} />
+    )
+
+    return (
+      <Row>
+        <Col sm="1/3">
+          <Form>
+          <h2 className='m2'>
+            {this.props.question}
+          </h2>
+          {answers}
+          <Button size="lg">Submit</Button>
+          </Form>
+        </Col>
+      </Row>
+    )
+  }
 }
 
 export default Question
