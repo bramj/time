67913a101e7d42f3498ac507c11df804fb15f043
Bram Jans <ik@bramjans.com>
2017-04-12 18:14:26 +0200

    Add elemental CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Question from './components/Question';
+import React, { Component } from 'react'
+import Question from './components/Question'
+import './styles.min.css'
 
 class App extends Component {
   render() {
